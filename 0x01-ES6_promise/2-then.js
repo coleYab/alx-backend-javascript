fe86d5ef1c58ab61f11b026a3145a755e0b27e75
coleYab <yabume123@gamil.com>
2024-08-07 21:07:54 +0300

    ~ added return statement

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,8 +1,7 @@
 export default function handleResponseFromAPI(promise) {
-  promise.then(
-    () => {
-      return { status: 200, body: 'success' };
-    },
-  ).catch(() => new Error());
-  console.log('Got a response from the API');
+  promise.then(() => ({ status: 200, body: 'success' }))
+    .catch(() => new Error())
+    .finally(() => {
+      console.log('Got a response from the API');
+    });
 }
