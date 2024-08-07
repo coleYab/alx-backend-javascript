ba6351763016f53ab345d362ba16de195812a0bf
coleYab <yabume123@gamil.com>
2024-08-07 21:59:05 +0300

    ~ added return statement

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,7 +1,7 @@
 import { uploadPhoto, createUser } from './utils';
 
 export default function handleProfileSignup() {
-  createUser().then(
+  return createUser().then(
     (user) => uploadPhoto().then((photo) => console.log(`${photo.body} ${user.firstName} ${user.lastName}`)),
   ).catch(() => console.log('Signup system offline'));
 }
