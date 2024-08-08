cb54215c19b24dd59a459885f00b03ec9041b1e2
coleYab <yabume123@gamil.com>
2024-08-09 00:52:02 +0300

    ~ eslint fix on 10-car.js

--- 0x02-ES6_classes/10-car.js
+++ 0x02-ES6_classes/10-car.js
@@ -1,41 +1,40 @@
 export default class Car {
-    constructor(brand, motor, color) {
-        this.brand = brand;
-        this.motor = motor;
-        this.color = color;
-    }
+  constructor(brand, motor, color) {
+    this.brand = brand;
+    this.motor = motor;
+    this.color = color;
+  }
 
-    get brand() {
-        return this._brand;
-    }
+  get brand() {
+    return this._brand;
+  }
 
-    get motor() {
-        return this._motor;
-    }
+  get motor() {
+    return this._motor;
+  }
 
-    get color() {
-        return this._color;
-    }
+  get color() {
+    return this._color;
+  }
 
-    set color(newColor) {
-        this._color = newColor;
-    }
+  set color(newColor) {
+    this._color = newColor;
+  }
 
-    set motor(newMotor) {
-        this._motor = newMotor;
-    }
+  set motor(newMotor) {
+    this._motor = newMotor;
+  }
 
-    set brand(newBrand) {
-        this._brand = newBrand;
-    }
+  set brand(newBrand) {
+    this._brand = newBrand;
+  }
 
-    static get [Symbol.species]() {
-        return this;
-    }
-    
-    cloneCar() {
-        const Species = this.constructor[Symbol.species];
-        return new Species();
-    }
-    
+  static get [Symbol.species]() {
+    return this;
+  }
+
+  cloneCar() {
+    const Species = this.constructor[Symbol.species];
+    return new Species();
+  }
 }
