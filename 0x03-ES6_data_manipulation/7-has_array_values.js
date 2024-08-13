ceaec0f647b7a148c3d31bc80e1df4167dcce2d8
coleYab <yabume123@gamil.com>
2024-08-13 18:43:59 +0300

    + more map and weak maps

--- 0x03-ES6_data_manipulation/7-has_array_values.js
+++ 0x03-ES6_data_manipulation/7-has_array_values.js
@@ -1,4 +1,3 @@
 export default function hasValuesFromArray(set, array) {
-  if (Array.isArray(array) === false) return set.has(array);
-  return array.reduce((a, b) => a && set.has(b), false);
+  return array.reduce((a, b) => a && set.has(b), true);
 }
