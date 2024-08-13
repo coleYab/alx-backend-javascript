28ff5c51619424e4d0f432d3df67e37e678e0137
coleYab <yabume123@gamil.com>
2024-08-13 15:48:46 +0300

    ~ fix spelling on columbia

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -2,5 +2,5 @@ export default function getListStudents() {
   const createObj = (id, firstName, location) => ({
     id, firstName, location,
   });
-  return [createObj(1, 'Gullame', 'San Francisco'), createObj(2, 'James', 'Colombia'), createObj(5, 'Serena', 'San Francisco')];
+  return [createObj(1, 'Gullame', 'San Francisco'), createObj(2, 'James', 'Columbia'), createObj(5, 'Serena', 'San Francisco')];
 }
