2df980235a69da553100df4ad60474d47a5c04a5
coleYab <yabume123@gamil.com>
2024-08-13 15:51:42 +0300

    ~ fixing spelling

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -2,5 +2,6 @@ export default function getListStudents() {
   const createObj = (id, firstName, location) => ({
     id, firstName, location,
   });
-  return [createObj(1, 'Gullame', 'San Francisco'), createObj(2, 'James', 'Columbia'), createObj(5, 'Serena', 'San Francisco')];
+  return [createObj(1, 'Guillaume', 'San Francisco'), createObj(2, 'James', 'Columbia'), createObj(5, 'Serena', 'San Francisco')];
 }
+
