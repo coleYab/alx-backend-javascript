0e6d7019e1a71d622590692d8d3df29e7fba4f77
coleYab <yabume123@gamil.com>
2024-08-13 18:27:01 +0300

    + more map and weak maps

--- 0x03-ES6_data_manipulation/10-update_uniq_items.js
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -2,4 +2,6 @@ export default function updateUniqueItems(map) {
   map.forEach((v, k) => {
     if (v === 1) map.set(k, 100);
   });
+
+  return map;
 }
--- 0x03-ES6_data_manipulation/100-weak.js
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -7,9 +7,7 @@ export function queryAPI(endpoint) {
 
   if (weakMap.has(endpoint)) {
     weakMap.set(endpoint, weakMap.get(endpoint) + 1);
-    return weakMap.get(endpoint);
   }
 
   weakMap.set(endpoint, 1);
-  return 1;
 }
