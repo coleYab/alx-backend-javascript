15a7b7eb72afef580577eb580a6e15e6ae88698f
coleYab <yabume123@gamil.com>
2024-08-15 18:16:33 +0300

    Type script

---
+++ 0x04-TypeScript/task_0/js/main.js
@@ -0,0 +1,16 @@
+"use strict";
+;
+var stu1 = {
+    firstName: 'Yeabsira',
+    lastName: 'Moges',
+    age: 20,
+    location: 'Addis Ababa, Ethiopia'
+};
+var stu2 = {
+    firstName: 'Samrawit',
+    lastName: 'Moges',
+    age: 24,
+    location: 'Addis Ababa, Ethiopia'
+};
+var students = [stu1, stu2];
+console.log(students);
