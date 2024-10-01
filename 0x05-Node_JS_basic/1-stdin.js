679f10504ac603fe89e55f8929db12b432e3ce19
coleYab <yabume123@gamil.com>
2024-10-01 23:54:09 +0300

    add: closing feature

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -7,3 +7,5 @@ r1.question('Welcome to Holberton School, what is your name?\n', (answer) => {
   console.log(`Your name is: ${answer}`);
   r1.close();
 });
+
+console.log("This important software is now closing")
