032516c2bfc784b2e0d9e4bc23e829a77e306011
coleYab <yabume123@gamil.com>
2024-10-01 23:55:49 +0300

    add: new shit

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -6,6 +6,6 @@ const r1 = readline.createInterface({ input, output });
 r1.question('Welcome to Holberton School, what is your name?\n', (answer) => {
   console.log(`Your name is: ${answer}`);
   r1.close();
+  console.log("This important software is now closing")
 });
 
-console.log("This important software is now closing")
